7281ba73579aae6fae25b8e09f169899d9e3d218
Fumedeme <1M.FurkanYilmaz@gmail.com>
2024-06-18 06:54:51 +0300

    Render markers on the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,14 @@
-import MapBox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import MapBox, {
+  Camera,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+  Images,
+} from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+import scooters from 'data/scooters.json';
+import pin from '~/assets/pin.png';
 
 MapBox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
@@ -7,6 +17,16 @@ const Map = () => {
     <MapView style={{ flex: 1 }}>
       <Camera followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={'default'} />
+
+      <ShapeSource
+        id="scooters"
+        shape={featureCollection(scooters.map((scooter) => point([scooter.long, scooter.lat])))}>
+        <SymbolLayer
+          id="scooter-icons"
+          style={{ iconImage: 'pin', iconSize: 0.5, iconAllowOverlap: true }}
+        />
+        <Images images={{ pin }} />
+      </ShapeSource>
     </MapView>
   );
 };
